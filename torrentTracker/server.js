0fd5566f8601b011b55ed33910d214dfc391c5b1
Magnus Lundstedt <magnus.lundstedt@infidyne.com>
2015-04-19 12:36:36 +0200

    Added folders for webtorrent demo and client/server implementations

---
+++ torrentTracker/server.js
@@ -0,0 +1,22 @@
+
+//Import jsFlow
+
+
+var jsFlow = require('./jsFlow');
+
+// this is needed to run jsFlow
+jsFlow.run("31bc728296d8da7e14e132k",{userId: 'tracker', sessionAuthURL: 'http://corslabs.com/jsflow', 
+                  debugMsg: true});
+
+//Dictionary med Hash / jsFlow ID
+var dictionaryOfUsers = {};
+
+// =================
+//  jsFlow handlers
+// =================
+
+jsFlow.addHandler('TBD', function (payload, from) {
+
+});
+
+//more...
---
+++ webtorrents/download.js
@@ -0,0 +1,23 @@
+var WebTorrent = require('webtorrent')
+
+var client = new WebTorrent()
+var magnetUri = '5a1052a8a744b6066a95ff0bbc878598f068b64f'
+
+client.add(magnetUri, function (torrent) {
+  // Got torrent metadata!
+  console.log('Torrent info hash:', torrent.infoHash)
+
+  torrent.files.forEach(function (file) {
+    // Get a url for each file
+    file.getBlobURL(function (err, url) {
+      if (err) throw err
+
+      // Add a link to the page
+      var a = document.createElement('a')
+      a.download = file.name
+      a.href = url
+      a.textContent = 'Download ' + file.name
+      document.body.appendChild(a)
+    })
+  })
+})
